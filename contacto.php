53900821bdec0da69bd877d248e75f77405d4725
Steffania <estefi5587@gmail.com>
2020-04-29 22:42:38 -0300

    contacto
    contacto captcha

--- contacto.php
+++ contacto.php
@@ -95,3 +95,25 @@ $estadoEnvio = $mail->Send();
 
 
 }
+<?php
+ 
+// grab recaptcha library
+require_once "recaptchalib.php";
+// your secret key
+$secret = "6LeZOvAUAAAAAPdOQHxrrGFv9YKmLKp9Q9WAamW6";
+ 
+// empty response
+$response = null;
+ 
+// check secret key
+$reCaptcha = new ReCaptcha($secret);
+// if submitted check response
+if ($_POST["g-recaptcha-response"]) {
+    $response = $reCaptcha->verifyResponse(
+        $_SERVER["REMOTE_ADDR"],
+        $_POST["g-recaptcha-response"]
+    );
+}
+<?php } 
+?>
+
